15fe132ca643ecaf9a184f547c5004985e7b4866
Manoj Ranabhat <manojranabhat17@gmail.com>
2025-03-04 09:30:42 +0545

    URL QUERY READER

---
+++ EncoderTest.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.URLEncoder;
+
+public class EncoderTest{
+    public static void main(String[] args) {
+        try {
+
+            System.out.println(URLEncoder.encode("This string has spaces","UTF-8"));
+            System.out.println(URLEncoder.encode("This*string*has*asrersiks","UTF-8"));
+            System.out.println(URLEncoder.encode("This%string%has%percents%signs","UTF-8"));
+            System.out.println(URLEncoder.encode("This+string+has+pluses","UTF-8"));
+            System.out.println(URLEncoder.encode("This/string/has/slashes","UTF-8"));
+            System.out.println(URLEncoder.encode("This\"string\"has\"quote\"marks","UTF-8"));
+            System.out.println(URLEncoder.encode("This: string: has: colons","UTF-8"));
+            System.out.println(URLEncoder.encode("This~string~ has~tildes","UTF-8"));
+            System.out.println(URLEncoder.encode("This (string)has(parenthesis)","UTF-8"));
+            System.out.println(URLEncoder.encode("This.string.has.periods","UTF-8"));
+            System.out.println(URLEncoder.encode("Thiséstringéhasénon non-ASCII characters","UTF-8"));
+
+        }catch(UnsupportedEncodingException ex){
+            throw new RuntimeException("Broken VM doesnot support UTF-8");
+        }
+            
+        }
+    }
